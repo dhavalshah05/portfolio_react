eb28efe920f5c4796d2e5ec350f715f6c39c3689
Dhaval Shah <dhaval@alphastack.in>
2022-06-11 13:04:11 +0530

    Added custom font

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 const App = () => {
-    return <h1>Hello World</h1>
+    return <h1 className={"font-lato"}>Hello World</h1>
 };
 
 ReactDOM.render(
