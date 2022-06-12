887ffdf58171fc5804dbc7d26bfefb4096fbf31b
Dhaval Shah <dhaval@alphastack.in>
2022-06-12 11:34:10 +0530

    Solved horizontal scroll added issue

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
     return (
-        <section className="bg-[#132C47] w-screen">
+        <section className="bg-[#132C47]">
             <div className="section-container flex flex-col items-center md:items-start">
                 <h2 className="section-header text-[#E1ECF3]">What I do?</h2>
                 <p className="mt-[20px] md:mt-[30px] text-[#E1ECF3]/80 text-[16px] md:text-[18px] font-lato font-regular text-center md:text-left tracking-[0.4px]">
