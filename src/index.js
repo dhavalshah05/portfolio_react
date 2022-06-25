92799d462292fa76acea93c880b09b8fc87530f8
Dhaval Shah <dhaval@alphastack.in>
2022-06-25 10:29:25 +0530

    Converting AppComponent to functional component

--- src/index.js
+++ src/index.js
@@ -7,36 +7,27 @@ import About from "./components/about/About";
 import Projects from "./components/projects/Projects";
 import Footer from "./components/footer/Footer";
 
-class AppComponent extends React.Component {
+const AppComponent = () => {
+    const [selectedItem, setSelectedItem] = React.useState('About');
 
-    state = {
-        selectedItem: 'About'
-    }
-
-    bindNavItem(navItem) {
+    const bindNavItem = (navItem) => {
         const elementToScroll = document.getElementById(navItem);
         const elementHeader = document.getElementById("nav-header");
-
         window.scroll({
             top: elementToScroll.offsetTop - elementHeader.clientHeight,
             behavior: "smooth"
-        })
+        });
+        setSelectedItem(navItem);
+    };
 
-        this.setState({
-            selectedItem: navItem
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <Header selectedItem={this.state.selectedItem} onNavItemClick={(navItem) => this.bindNavItem(navItem)} />
-                <About />
-                <Projects />
-                <Footer />
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Header selectedItem={selectedItem} onNavItemClick={(navItem) => bindNavItem(navItem)} />
+            <About />
+            <Projects />
+            <Footer />
+        </div>
+    );
 }
 
 const rootElement = document.getElementById("root");
